ce40f9e71b70d8c402dce0ab544f969d02fd2b20
Yao-Lun Yang <allenya@gmail.com>
2018-04-10 12:18:04 -0500

    fix bugs

--- run_pylime.py
+++ run_pylime.py
@@ -8,7 +8,9 @@ model_list = ascii.read('/scratch/LIMEmods/pylime/lime/YLY/lime_models/model_lis
 outdir_base = '/scratch/LIMEmods/pylime/lime/YLY/run/'
 pylime = '/scratch/LIMEmods/pylime/lime/pylime'
 
-for m in model_list['model_name'][-1]:
+for m in model_list['model_name']:
+    if m != 6:
+        continue
     # use the config file as the communication between model.py and user-defined model list
     foo = open('/scratch/LIMEmods/pylime/lime/YLY/lime_models/lime_config.txt', 'w')
 
