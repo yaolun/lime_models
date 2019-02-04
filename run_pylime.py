d330fc567929859590371506e0932ca1ee4e6a5b
Yao-Lun Yang <allenya@gmail.com>
2019-02-04 21:40:30 +0100

    try to fix a bug

--- run_pylime.py
+++ run_pylime.py
@@ -135,7 +135,7 @@ for i, m in enumerate(model_list['model_name']):
         print('Start running model '+str(m))
         log = open(outdir+'pylime.log','w')
         err = open(outdir+'pylime.err','w')
-        run = call([pylime, 'model.py'], stdout=log, stderr=err)
+        run = call([pylime, 'model.py'], stdout=log, stderr=err, shell=True)
 
         if not os.path.exists(outdir+'grid5'):
             print('grid files not found.  pylime probably failed.')
