38c7184db3babd777dafbce8f8c2fb2a1c970d90
Yao-Lun Yang <allenya@gmail.com>
2019-02-07 23:03:29 +0100

    debug

--- Hyperion2LIME.py
+++ Hyperion2LIME.py
@@ -75,6 +75,7 @@ class Hyperion2LIME:
                 for i in range(self.ntheta):
                     dvr = abs((self.vr2d[1:,i] - self.vr2d[:-1,i])/self.vr2d[1:,i])
                     break_pt = self.xr[1:][(dvr > 0.1) & (self.xr[1:] > 1e-3) & (self.xr[1:] < 1-2e-3)]
+                    print(dvr[(dvr > 0.1) & (self.xr[1:] > 1e-3) & (self.xr[1:] < 1-2e-3)])
                     print(break_pt)
                     if len(break_pt) > 0:
                         offset = self.vr2d[(self.xr < break_pt),i].max() - self.vr2d[(self.xr > break_pt),i].min()
