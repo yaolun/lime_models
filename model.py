e5e697c32051ad7af52e9e4001f858909b883367
Yao-Lun Yang <allenya@gmail.com>
2019-02-07 09:35:59 +0100

    debug

--- model.py
+++ model.py
@@ -15,6 +15,7 @@ from par_classes import *
 
 # import results from Hyperion and TSC calculations
 import os
+import numpy
 # user-dependent
 sys.path.append(os.path.expanduser('~')+'/anaconda/lib/python2.7/site-packages/')
 import Hyperion2LIME as h2l
