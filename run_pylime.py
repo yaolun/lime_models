2c6141e900f73af625cbc8b2d41ddf7ed3270c97
Yao-Lun Yang <allenya@gmail.com>
2019-03-13 18:10:10 -0500

    bug fix

--- run_pylime.py
+++ run_pylime.py
@@ -18,7 +18,7 @@ parser.add_argument('--no_image', action='store_true',
 parser.add_argument('--vr_factor', default='1.0', help='artifically reduce the radial velocity by a factor')
 parser.add_argument('--vr_offset', default='0.0', help='additional offset added to vr.  (vr < 0 for infall, thus vr_offset < 0 would increase the infall velocity.)')
 parser.add_argument('--age', help='one-time change to the age for the current run [unit: yr]')
-parser.add_argument('--omega', help='one-time change to the oemga for the current run [unit: s-1].  Note "" is necessary for a correct interpration of scientific notation.')
+parser.add_argument('--omega', type=float, help='one-time change to the oemga for the current run [unit: s-1].')
 parser.add_argument('--gridding', action='store_true', help='Only run LIME for the gridding purpose.  It will use the "gridding" version in getDensity(), which takes the density profile from the TSC-Fortran output with no cavity.')
 parser.add_argument('--dry_run', action='store_true', help='Test run the program until the point where LIME will be executed.')
 parser.add_argument('--hybrid_tsc', action='store_true', help='Option to have an angular momentum consered envelope inside the centrifugal radius')
